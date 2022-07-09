f16a5a9a615639d917f7a1f43852d06542f17c72
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-08 18:38:48 -0700

    getting a error: 400 invalid_request
    missing required parameter: client_id

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,3 +1,46 @@
+// const { google } = require("googleapis");
+// const OAuth2 = google.auth.OAuth2;
+// const calendar = google.calendar("v3");
+
+// const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+// const credentials = {
+//   client_id: process.env.CLIENT_ID,
+//   project_id: process.env.PROJECT_ID,
+//   client_secret: process.env.CLIENT_SECRET,
+//   calendar_id: process.env.CALENDAR_ID,
+//   auth_uri: "https://accounts.google.com/o/oauth2/auth",
+//   token_uri: "https://oauth2.googleapis.com/token",
+//   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+//   redirect_uris: ["https://amerikanpsyko.github.io/meet/"],
+//   javascript_origins: ["https://amerikanpsyko.github.io", "http://localhost:3000"],
+// }
+
+// const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+// const oAuth2Client = new google.auth.OAuth2(
+//   client_id,
+//   client_secret, 
+//   redirect_uris[0]
+// );
+
+// module.exports.getAuthURL = async () => {
+//   const authURL = oAuth2Client.generateAuthUrl({
+//     access_type: "offline",
+//     scope: SCOPES,
+//   });
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//     },
+//     body: JSON.stringify({
+//       authURL: authURL, 
+//     }),
+//   };
+
+// };
+
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
@@ -19,7 +62,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://amerikanpsyko.github.io/meet/"],
+  redirect_uris: ["https://amerikanpsyko.github.io/meet"],
   javascript_origins: ["https://amerikanpsyko.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
