2cc73d9e96a5ca90f373f6bf7c75ff1a4aedafcd
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-18 06:33:09 -0700

    stopping for now
    aws account suspended pending update on security details

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import { getEvents, extractLocations } from "./api";
 import "./nprogress.css";
-import { Container, Row, Col } from 'react-bootstrap/';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NumberOfEvents from "./NumberOfEvents";
 
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
+    numberOfEvents: 15,
   };
 
-  updateEvents = (location) => {
+  updateEvents = (location, eventCount) => {
+    if (eventCount === undefined) {
+        eventCount = this.state.numberOfEvents;
+    } else(
+        this.setState({ numberOfEvents: eventCount })
+    )
+    if (location === undefined) {
+        location = this.state.locationSelected;
+    }
     getEvents().then((events) => {
-      const locationEvents =
-        location === "all"
-          ? events
-          : events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents,
-      });
-    });
-  };
+        let locationEvents = location === "Everywhere" ?
+            events :
+            events.filter((event) => event.location === location);
+        this.setState({
+            events: locationEvents.slice(0, eventCount),
+            numberOfEvents: eventCount,
+            locationSelected: location,
+        });
+    })
+}
 
   componentDidMount() {
     this.mounted = true;
@@ -44,6 +56,7 @@ class App extends Component {
         <Container>
         <Row className="d-flex justify-content-center align-item-center p-3 m-3">
           <Col md={6} className="d-flex flex-column align-items-center justify-content-center p-5">
+          <NumberOfEvents updateEvents = { this.updateEvents } />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
