2bd0abaeb41753a90a02b058c844e2f833ab9f97
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-19 12:39:15 -0700

    added atatus
    everything working as expected

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('874d5dfd08584a408d7658efba9be4cd').install();
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
