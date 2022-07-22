94e9394ba19aad991fddbcca1f4e4fc1232c0c17
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-21 19:37:27 -0700

    almost there. just need to figure out error
    null event for infoText creating error and no rendering

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
+// import { ErrorAlert } from "./Alert";
 
 class NumberofEvents extends Component {
 
 
   handleInputChanged = (event) => {
     const selectedValue = parseInt(event.target.value);
+
     if (selectedValue <= 20 && selectedValue > 0) {
       this.setState({
-        numberOfEvents: selectedValue
+        numberOfEvents: 20,
+        infoText: 'Please select a number from 1-20'
+
       });
     } else {
       this.setState({
-        numberOfEvents: 20,
-        Error: 'Please select a number greater than 0.'
+        numberOfEvents: selectedValue,
+        infoText: ''
       })
     }
     this.props.updateEvents(undefined, selectedValue);
@@ -21,6 +25,7 @@ class NumberofEvents extends Component {
   render() {
     const { numberOfEvents } = this.props;
     return <div className="event-count">
+      {/* <ErrorAlert text={this.state.infoText} /> */}
       <p>Number of Events:</p>
       <input className="event-number" type="number" min="1" max="100" value={numberOfEvents} onChange={this.handleInputChanged}>
 
