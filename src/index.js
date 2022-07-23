7046bddd0faa9f89e1f67236fc0927cac5aea7de
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-23 06:47:08 -0700

    4.8 final
    finished all tasks with no issues or errors. yaay!!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import * as atatus from 'atatus-js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+// import * as atatus from 'atatus-spa';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// atatus.config('ad111dd8546842279652b47f7dfb6f94').install();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
