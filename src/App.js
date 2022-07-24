43c50361577f6ca54b6dd3a435f9841b29023090
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-07-24 14:18:36 -0700

    added responsive container
    everything working. Final push

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 
 import NumberofEvents from "./NumberOfEvents";
@@ -97,22 +98,20 @@ class App extends Component {
           numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
         />
-        <ScatterChart
-          width={800}
-          height={400}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-        >
-          <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city"  />
-          <YAxis type="number" dataKey="number" name="number of events"  />
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Scatter data={this.getData()} fill="#8884d8" />
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
           </ScatterChart>
+        </ResponsiveContainer>
         <EventList
           events={this.state.events}
           updateEvents={this.updateEvents}
